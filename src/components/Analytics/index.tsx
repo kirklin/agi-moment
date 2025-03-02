8fa595d023b9a9f3cc10230e8a9e1597a0c65f83
Kirk Lin <linkirk@163.com>
2025-03-02 22:33:14 +0800

    feat: add Google Analytics

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import { getClientConfig } from "~/config/client";
 
 const Vercel = dynamic(() => import("./Vercel"), { ssr: false });
+const GoogleAnalytics = dynamic(() => import("./GoogleAnalytics"), { ssr: false });
 
 const { ANALYTICS_VERCEL } = getClientConfig();
 
@@ -10,6 +11,7 @@ function Analytics() {
   return (
     <>
       {ANALYTICS_VERCEL && <Vercel />}
+      <GoogleAnalytics />
     </>
   );
 }
